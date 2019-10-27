872d134d7599562812d703982276b50391855aa2
Jakov764 <55740028+Jakov764@users.noreply.github.com>
2019-10-27 20:07:31 +0100

    Add files via upload

---
+++ cvekŠpraljaJakovDomaćaZadaća/cvekŠpraljaJakovDomaćaZadaća/Program.cs
@@ -0,0 +1,48 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cvekŠpraljaJakovPetlje
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float unosOcjene;
+            float kolikoOcjena;
+            float zbroj;
+            float prosjekOcjena;
+
+
+            Console.WriteLine("Koliko ocjena zelite upisati: ");
+
+            kolikoOcjena = Console.Read();
+
+            for (float i = 0; i < kolikoOcjena; i++) ;
+            {
+                Console.WriteLine("Unesi ocjenu (1-5): ");
+
+                unosOcjene = Console.Read();
+
+                Console.WriteLine(unosOcjene);
+
+                for (unosOcjene >= 2; unosOcjene < 6;) ;
+                {
+                    zbroj += unosOcjene;
+                    prosjekOcjena = zbroj / kolikoOcjena;
+                }
+                if (unosOcjene = 1)
+                {
+                    prosjekOcjena = 1;
+                }
+            }
+            Console.WriteLine("Prosjek ocjena je: " + prosjekOcjena);
+
+
+            Console.ReadKey();
+        }
+    }
+}
